fb645ea6ca54437792c2197d377c88fdd5e0c386
Abdullrhman Aljasser <abdullrhmanaljasser@gmail.com>
2019-05-21 23:05:32 -0400

    Revert "Update Client.java"
    This reverts commit 18bbea885c3400b5a884140ab20e27f1d6ea2ff9.

--- src/program/Client.java
+++ src/program/Client.java
@@ -1,182 +1,203 @@
-//
-import java.io.*;
-import java.net.*;
-import java.util.*;
+package program;
 
-public class Simulator extends Thread{
-   
-   private DatagramSocket listeningSocket;
-   private DatagramPacket packet;
-   
-   public Simulator() throws Exception
-   {
-      this.listeningSocket = new DatagramSocket(29);
-   }
+import java.io.FileNotFoundException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
 
-   public static void main(String arg[] )
-   {
-       try
-       {
-           (new Simulator()).start();
-       }catch(Exception e){ System.out.println(e);}
+import javax.swing.JOptionPane;
+ 
+import utilities.FILEUtil;
+import utilities.Request;
+import utilities.TFTPUtil;
+import utilities.*;
+import utilities.packets.*;
 
-   }
+public class Client {
 
-   public void run(){
-       this.listen();
-   }
+	private static final String PATH = "C:\\Users\\AyeJay\\Desktop\\files\\";
 
-   private void listen()
-   {
-        try {
-            System.out.println("Simulator is Listening on Port 29: Waiting for packet.");
-           while(true)
-           {
-            //listening to client queries
-            byte[] data = new byte[512];
-            packet = new DatagramPacket(data, data.length);
+	private static final int MAX_CAPACITY = 512;
+	
+	private   int SERVER_PORT ;
+	private   InetAddress SERVER_ADDRESS;
+	private InetAddress SIMULATOR_ADDRESS;
+	private int SIMULATOR_PORT;
 
-            this.listeningSocket.receive(this.packet);
+	// datagram socket used to send/receive packets
+	private DatagramSocket sendReceiveSocket;
 
-            //create a thread that will handle the request
-            (new RequestHandler(this.packet)).start();
-           }
-        } catch (Exception se) 
-        {
-            se.printStackTrace();
-            System.exit(1);
-         }
-   }
+	// constructor
+	public Client() {
+		sendReceiveSocket = TFTPUtil.datagramSocket();
+		SERVER_PORT = 69;
+		SIMULATOR_PORT = 29;
+	    try {
+			SERVER_ADDRESS = InetAddress.getLocalHost();
+			SIMULATOR_ADDRESS = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	
+	}
 
-   private class RequestHandler extends Thread
-   {
-        private DatagramSocket clientSocket;
-        private DatagramSocket serverSocket;
+	public void sendReceive() {
+		// creates a request Object with the type of request entered by the user
+		Request request =createRequest(JOptionPane.showInputDialog("File transfer operation(Write or Read):"));
+		// saves the filename that the user enters
+		String filename = JOptionPane.showInputDialog("Enter the file name :");
+		// saves the mode that the user enters.
+		String mode =JOptionPane.showInputDialog("Enter the mode :").toLowerCase();
+		// creates a requestPacket
+		RequestPacket RPacket = new RequestPacket(request, filename, mode);
 
-        private DatagramPacket toServerPacket;
-        private DatagramPacket toClientPacket;
-        private DatagramPacket fromClientPacket;
-        private DatagramPacket fromServerPacket;
+		if (request.getType().equalsIgnoreCase("read")) {
+			establishRRQ(RPacket);
+			readFile(RPacket);
+			
+		} else if (request.getType().equalsIgnoreCase("write")) {
 
-        InetSocketAddress client;
-        InetSocketAddress server = new InetSocketAddress("localhost",69);
+			int port = establishWRQ(RPacket);
 
-        RequestHandler(DatagramPacket clientPacket) throws Exception
-        {
-            //construct the packet to send to the server
-            this.toServerPacket = new DatagramPacket(clientPacket.getData(),clientPacket.getData().length, this.server);
+			if (port != -1) {
+				writeFile(RPacket, port);
+			} else {
+				System.out.println("something went wrong, could not connect to server");
+			}
+		} else
+			System.out.println("Could not contact server");
 
-            //create a client profile
-            this.client = new InetSocketAddress(clientPacket.getAddress(), clientPacket.getPort());
-           
-            //create sockets
-            this.clientSocket = new DatagramSocket();
-            this.serverSocket = new DatagramSocket();
-        }
+		sendReceiveSocket.close(); // closes the socket
+	}
 
-        public void run()
-        {
-           this.initializeTransaction();
-        }
-        
-        private void initializeTransaction()
-        {
-            try{
-                while(true) //infinite loop
-                {
-            //send the packet to the server
-                sendToServer();
-            //wait for response from the server
-                getServerResponse();
-            //construct a client packet
-                this.toClientPacket = new DatagramPacket(this.fromServerPacket.getData(), this.fromServerPacket.getData().length, this.client);
-            //send the response to the client
-                sendToClient();
-            //wait for response from the client
-                getClientResponse();
-                this.toServerPacket = new DatagramPacket(this.fromClientPacket.getData(), this.fromClientPacket.getData().length, this.server);
-                }
-            }catch(Exception e){
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
+	public int establishWRQ(RequestPacket rp) {
+		
+		int port = -1;
+		
+		rp.setDatagramPacket(SERVER_ADDRESS, SIMULATOR_PORT);		
+		
+		TFTPUtil.send(sendReceiveSocket, rp.getDatagramPacket(), "Trying to connect to server...");
 
-        private void sendToClient() throws Exception
-        {
-            System.out.println("Simulator: Transfering to Client:");
-            System.out.println("To host: " + this.toClientPacket.getAddress());
-            System.out.println("Destination host port: " + this.toClientPacket.getPort());
-            int len = this.toClientPacket.getLength();
-            System.out.println("Length: " + len);
-            System.out.println("Containing: ");
-            for (int j=0;j<len;j++) {
-            System.out.println("byte " + j + " " + this.toClientPacket.getData()[j]);
-            }
-            
-            this.clientSocket.send(this.toClientPacket);
-        }
+		DatagramPacket response = TFTPUtil.datagramPacket(4);
 
+		TFTPUtil.receive(sendReceiveSocket, response, "Waiting for ACK...");
 
-        private void sendToServer() throws Exception
-        {
-         System.out.println("Simulator: Transfering to Server:");
-         System.out.println("To host: " + this.toServerPacket.getAddress());
-         System.out.println("Destination host port: " + this.toServerPacket.getPort());
-         int len = this.toServerPacket.getLength();
-         System.out.println("Length: " + len);
-         System.out.println("Containing: ");
-         for (int j=0;j<len;j++) 
-         {
-            System.out.println("byte " + j + " " + this.toServerPacket.getData()[j]);
-         }
-         this.serverSocket.send(this.toServerPacket);
-        }
+		ACKPacket ack = new ACKPacket(response.getData());
 
+		byte[] ackBytes = ack.getID();
+		int ackBN = ack.getIntBN();
 
-        private void getServerResponse() throws Exception
-        {
-         this.serverSocket.receive(this.fromServerPacket);
-         // Process the received datagram.
-         System.out.println("Simulator: Received from Server:");
-         System.out.println("From host: " + this.fromServerPacket.getAddress());
-         int clientPort = this.fromServerPacket.getPort();
-         System.out.println("Host port: " + clientPort);
-         int len = this.fromServerPacket.getLength();
-         System.out.println("Length: " + len);
-         System.out.println("Containing: " );
-         // print the bytes
-         for (int j=0;j<len;j++) 
-         {
-            System.out.println("byte " + j + " " + this.fromServerPacket.getData()[j]);
-         }
+		if (ackBytes[0] == 0 && ackBytes[1] == 4 && ackBN == 0)// byte[1] must be 4 not 1
+			port = response.getPort();
 
-        }
+		return port;
+	}
 
+	public void writeFile(RequestPacket rp, int port) {
+		System.out.println("WriteFile Method...");
 
-        private void getClientResponse() throws Exception
-        {
-            this.clientSocket.receive(this.fromClientPacket);
+		String path = PATH + rp.getFilename();
+		FILEUtil file = null;
+		try {
+			file = new FILEUtil(path);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-         // Process the received datagram.
-         System.out.println("Simulator: Received from Client:");
-         System.out.println("From host: " + this.fromClientPacket.getAddress());
-         int clientPort = this.fromClientPacket.getPort();
-         System.out.println("Host port: " + clientPort);
-         int len = this.fromClientPacket.getLength();
-         System.out.println("Length: " + len);
-         System.out.println("Containing: " );
-         // print the bytes
-         for (int j=0;j<len;j++) 
-         {
-            System.out.println("byte " + j + " " + this.fromClientPacket.getData()[j]);
-         }
+		byte[][] data = file.getData();
+		
+		for (int i = 0; i < data.length; i++) {
+			DataPacket dp = new DataPacket(i + 1, data[i]);
 
-        }
+			dp.setDatagramPacket(SERVER_ADDRESS, port);
+			
+			TFTPUtil.send(sendReceiveSocket, dp.getDatagramPacket(), "Sending Packet #" + dp.getIntBN());
 
-   }
+			if( i != data.length -1) {
+				DatagramPacket ack = TFTPUtil.datagramPacket(4);
+		
+				TFTPUtil.receive(sendReceiveSocket, ack, "Waiting for ACK...");
+		
+				ACKPacket ackPacket = new ACKPacket(ack.getData());
+		
+				System.out.println("Got ACK PACKET #" + ackPacket.getIntBN());
+			}
 
+		}
 
+		System.out.println("FINISH WRITING...");
+
+	}
+
+	public void establishRRQ(RequestPacket rp) {
+		rp.setDatagramPacket(SERVER_ADDRESS, SIMULATOR_PORT);
+		
+		TFTPUtil.send(sendReceiveSocket, rp.getDatagramPacket(), "Trying to connect to server...");
+	}
+	
+	public void readFile(RequestPacket rp) {
+		boolean is512 = true;
+		byte[][] data = new byte[0][];
+		//byte[][] data = new byte[512][508];
+		//int counter =0;
+		
+		while(is512) {
+			DatagramPacket dgp = TFTPUtil.datagramPacket(MAX_CAPACITY);
+			
+			TFTPUtil.receive(sendReceiveSocket, dgp, "Waiting for DataACK...");
+			
+			DataPacket dataPacket = new DataPacket(Arrays.copyOfRange(dgp.getData(),0,dgp.getLength()));
+			
+			data = ArrayUtil.pushBidimensional(data, dataPacket.getData());
+			//data[counter] = Arrays.copyOfRange(dgp.getData(),4,dgp.getLength());
+			//System.out.println(dgp.getLength());
+			//counter++;
+			
+			ACKPacket ack = new ACKPacket(dataPacket.getIntBN());
+			
+			ack.setDatagramPacket(SERVER_ADDRESS, dgp.getPort());
+			
+			TFTPUtil.send(sendReceiveSocket, ack.getDatagramPacket(), "Sending ACK # " + dataPacket.getIntBN());
+			
+			if(dgp.getLength() < 512) {
+				is512 = false;
+				System.out.println("FINISHED Reading...");
+			}
+		}
+		//data = Arrays.copyOfRange(data, 0, counter);
+		FILEUtil file = new FILEUtil(data);
+		//System.out.println(rp.getFilename());
+		file.saveFile(PATH + rp.getFilename());
+		
+	}
+	
+	public void sendPacket(Packet packet, int port) {
+				
+	}
+	
+	public Request createRequest(String type) {
+		try {
+			return new Request(type);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(null, "Wrong type of request, try typing 'write' or 'read'", "ERROR",
+					JOptionPane.ERROR_MESSAGE);
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return null;
+	}
+
+	/**
+	 * Main method of Client class
+	 */
+	public static void main(String args[]) {
+		Client client = new Client();
+		client.sendReceive();
+	}
 }
-
-
