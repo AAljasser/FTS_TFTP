caf924f4181c3e24259bae3e72b5d5751a5c7d03
abemba <42939449+abemba@users.noreply.github.com>
2019-05-24 01:08:44 -0400

    Update Simulator.java
    added options to delay, consume and duplicate packets

--- src/program/Simulator.java
+++ src/program/Simulator.java
@@ -1,7 +1,3 @@
-/*
- * WORKS
- */
-
 //
 import java.io.*;
 import java.net.*;
@@ -29,87 +25,32 @@ public class Simulator extends Thread{
    }
 
    public void run(){
-
         this.listen();
-
-   }
-
-   private class Listener extends Thread
-   {
-    DatagramSocket socket;
-    DatagramPacket packet;
-    Listener() throws Exception
-    {
-        this.socket = new DatagramSocket(69);
-    }
-
-    public void run()
-    {
-        this.listen();
-    }
-
-    private void listen() 
-    {
-        try{
-         byte[] buff = new byte[512];
-         this.packet = new DatagramPacket(buff, buff.length);
-         System.out.println("Server: Listening on port 69");
-         while(true)
-         {
-             //Thread.sleep(3000);
-             this.socket.receive(packet);
-             System.out.println("Server Received packet");
-         }
-         }catch(Exception e){ System.out.println(e);}
-     }
-   }
-
-
-   private class Client extends Thread
-   {
-       DatagramSocket socket;
-       DatagramPacket packet;
-       Client() throws Exception
-       {
-           this.socket = new DatagramSocket();
-       }
-
-       public void run()
-       {
-           this.transfer();
-       }
-
-       private void transfer()
-       {
-           try{
-            byte[] buff = new byte[512];
-            this.packet = new DatagramPacket(buff, buff.length, InetAddress.getLocalHost(),29);
-            while(true)
-            {
-                Thread.sleep(3000);
-                this.socket.send(packet);
-                System.out.println("Sent a packet");
-            }
-            }catch(Exception e){ System.out.println(e);}
-        }
    }
 
 
    private void listen()
    {
+       boolean running = false;
         try {
-            //starting server
-            Listener server = new Listener();
-            server.start();
-
-            //starting Client
-            Client client = new Simulator.Client();
-            client.start();
-
-
-            System.out.println("Simulator is Listening on Port 29: Waiting for packet.");
            while(true)
            {
+            //show options
+            System.out.println("\t----------Welcome to the Simulator-------------\n\t Please select an option below then press <enter>\n\t\t1 DELAY PACKET \n\t\t2 LOSS PACKET \n\t\t3 DUPLICATE PACKETS \n");
+            //user selects an option
+            String pick = System.console().readLine();
+            System.out.println("You entered: "+pick);
+           //user select the starting packet number
+           System.out.println("\n Please insert the starting packet # to effect then press <enter> \n");
+            String startNumber = System.console().readLine();
+            System.out.println("You entered: "+startNumber);
+           //user select the finishing packet
+           System.out.println("\n Please insert the finishing packet # to effect then press <enter> \n");
+           String endNumber = System.console().readLine();
+            System.out.println("You entered: "+endNumber);
+           //ends
+           System.out.println("\n Packet #"+startNumber+" to #"+endNumber+" will experience OPTION "+pick+"\n");
+
             //listening to client queries
             byte[] data = new byte[512];
             packet = new DatagramPacket(data, data.length);
@@ -117,7 +58,8 @@ public class Simulator extends Thread{
             this.listeningSocket.receive(this.packet);
 
             //create a thread that will handle the request
-            (new RequestHandler(this.packet)).start();
+            (new RequestHandler(this.packet,pick,Integer.parseInt(startNumber),Integer.parseInt(endNumber))).start();
+            return;
            }
         } catch (Exception se) 
         {
@@ -136,6 +78,12 @@ public class Simulator extends Thread{
         private DatagramPacket fromClientPacket;
         private DatagramPacket fromServerPacket;
 
+        private String clientPacketEffect = "0";
+        private int clientPacketEffectLowerRange = 0;
+        private int clientPacketUpperRange = 0;
+
+        private int clientPacketSentCounter =0;
+
         InetSocketAddress client;
         InetSocketAddress server = new InetSocketAddress("localhost",69);
 
@@ -145,11 +93,23 @@ public class Simulator extends Thread{
             this.toServerPacket = new DatagramPacket(clientPacket.getData(),clientPacket.getData().length, this.server);
 
             //create a client profile
-            this.client = new InetSocketAddress(clientPacket.getAddress(), clientPacket.getPort());
+            this.client = new InetSocketAddress("localhost", clientPacket.getPort());
+
+            //store the packet
+            this.fromClientPacket = clientPacket;
+            this.fromClientPacket.setSocketAddress(this.client);
            
             //create sockets
             this.clientSocket = new DatagramSocket();
             this.serverSocket = new DatagramSocket();
+        }
+
+        RequestHandler(DatagramPacket clientPacket, String type, int startRange, int endRange) throws Exception
+        {
+            this(clientPacket);
+            this.clientPacketEffect = type;
+            this.clientPacketUpperRange = endRange;
+            this.clientPacketEffectLowerRange = startRange;
         }
 
         public void run()
@@ -182,31 +142,50 @@ public class Simulator extends Thread{
 
         private void sendToClient() throws Exception
         {
-            System.out.println("Simulator: Transfering to Client:");
-            System.out.println("To host: " + this.toClientPacket.getAddress());
-            System.out.println("Destination host port: " + this.toClientPacket.getPort());
+            System.out.println("->Simulator: Transfering Packet #"+this.clientPacketSentCounter+" to Client:");
+            System.out.println("\t To host: " + this.toClientPacket.getAddress());
+            System.out.println("\t Destination host port: " + this.toClientPacket.getPort());
             int len = this.toClientPacket.getLength();
-            System.out.println("Length: " + len);
-            System.out.println("Containing: ");
+            System.out.println("\t Length: " + len);
+            /* System.out.println("Containing: ");
             for (int j=0;j<len;j++) {
             System.out.println("byte " + j + " " + this.toClientPacket.getData()[j]);
-            }
-            
+            } */
+
             this.clientSocket.send(this.toClientPacket);
         }
 
 
         private void sendToServer() throws Exception
         {
-         System.out.println("Simulator: Transfering to Server:");
-         System.out.println("To host: " + this.toServerPacket.getAddress());
-         System.out.println("Destination host port: " + this.toServerPacket.getPort());
+         this.clientPacketSentCounter++;
+         
+         System.out.println("->Simulator: Packet #"+this.clientPacketSentCounter+" Transfering to Server:");
+         System.out.println("\t To host: " + this.toServerPacket.getAddress());
+         System.out.println("\t Destination host port: " + this.toServerPacket.getPort());
          int len = this.toServerPacket.getLength();
-         System.out.println("Length: " + len);
-         System.out.println("Containing: ");
+         System.out.println("\t Length: " + len);
+        /*  System.out.println("Containing: ");
          for (int j=0;j<len;j++) 
          {
             System.out.println("byte " + j + " " + this.toServerPacket.getData()[j]);
+         } */
+         if(this.clientPacketEffectLowerRange <= this.clientPacketSentCounter && this.clientPacketUpperRange >= this.clientPacketSentCounter )
+         {
+            switch(this.clientPacketEffect)
+            {
+                case "1":
+                    System.out.println("\t ****DELAYING SENDING PACKET #"+this.clientPacketSentCounter+" TO SERVER");
+                    Thread.sleep(9000);
+                    break;
+                case "2":
+                    System.out.println("\t ****LOSING PACKET #"+this.clientPacketSentCounter+" FROM CLIENT");
+                    return;
+                case "3":
+                    System.out.println("\t ****SENDING A DUPLICATE OF PACKET #"+this.clientPacketSentCounter+" TO SERVER");
+                    this.serverSocket.send(this.toServerPacket);
+                    break;
+            }
          }
          this.serverSocket.send(this.toServerPacket);
         }
@@ -214,43 +193,45 @@ public class Simulator extends Thread{
 
         private void getServerResponse() throws Exception
         {
+            System.out.println("->Simulator: Waiting For Server Response");
             byte[] buff = new byte[512];
             this.fromServerPacket = new DatagramPacket(buff, buff.length);
          this.serverSocket.receive(this.fromServerPacket);
          // Process the received datagram.
-         System.out.println("Simulator: Received from Server:");
-         System.out.println("From host: " + this.fromServerPacket.getAddress());
+         System.out.println("->Simulator: Received from Server:");
+         System.out.println("\t From host: " + this.fromServerPacket.getAddress());
          int clientPort = this.fromServerPacket.getPort();
-         System.out.println("Host port: " + clientPort);
+         System.out.println("\t Host port: " + clientPort);
          int len = this.fromServerPacket.getLength();
-         System.out.println("Length: " + len);
-         System.out.println("Containing: " );
+         System.out.println("\t Length: " + len);
+         /* System.out.println("Containing: " );
          // print the bytes
          for (int j=0;j<len;j++) 
          {
             System.out.println("byte " + j + " " + this.fromServerPacket.getData()[j]);
-         }
+         } */
 
         }
 
 
         private void getClientResponse() throws Exception
         {
+            System.out.println("->Simulator: Waiting For CLIENT Response");
             this.clientSocket.receive(this.fromClientPacket);
 
          // Process the received datagram.
-         System.out.println("Simulator: Received from Client:");
-         System.out.println("From host: " + this.fromClientPacket.getAddress());
+         System.out.println("->Simulator: Received from Client:");
+         System.out.println("\t From host: " + this.fromClientPacket.getAddress());
          int clientPort = this.fromClientPacket.getPort();
-         System.out.println("Host port: " + clientPort);
+         System.out.println("\t Host port: " + clientPort);
          int len = this.fromClientPacket.getLength();
-         System.out.println("Length: " + len);
-         System.out.println("Containing: " );
+         System.out.println("\t Length: " + len);
+         /* System.out.println("Containing: " );
          // print the bytes
          for (int j=0;j<len;j++) 
          {
             System.out.println("byte " + j + " " + this.fromClientPacket.getData()[j]);
-         }
+         } */
 
         }
 
@@ -258,7 +239,5 @@ public class Simulator extends Thread{
 
 
 }
-
-
 
 
