4abac3a9b5389608eee4fcd5e94f4f9683137ca5
Jose Franco <josefrancojimenez@cmail.carleton.ca>
2019-06-13 00:30:34 -0400

    it5 changes

--- src/program/Client.java
+++ src/program/Client.java
@@ -17,10 +17,11 @@ import utilities.packets.*;
 
 public class Client {
 	
-	protected static final boolean VERBOSE = true;
-	protected static final String PATH = "D:\\Users\\Abdullrhman Aljasser\\Documents\\GitHub\\FTS_TFTP\\files\\";
+	protected static final String PATH = "C:\\Jose\\Java\\files\\";
+	protected static final boolean VERBOSE = true;	
 	protected static final int MAX_CAPACITY = 512;
-	protected static final int SERVER_PORT = 29;
+	protected static final int SERVER_PORT = 69;
+	protected static final int SIMULATOR_PORT = 29;
 
 	protected int serverPort;
 	//used to check for error 5, this is set as soon as the server responds and remains constant
@@ -31,35 +32,36 @@ public class Client {
 	
 	private String filename;
 	private String mode;
+	private String hostName;
 	private Request request;
 	private Scanner scanner = new Scanner(System.in);
+	private int port;
 
 	// constructor
 	public Client() {		
-		try {
-			serverAddress = InetAddress.getLocalHost();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-
+		
 	}
 
 	public void sendReceive() {
 		String shutdown = null;
 		
-		do {
-		reset();
+		System.out.println("CLIENT");
+		setServerAddress();
+		System.out.println("Type 1 for Normal mode or type anything else for Simulator mode ");
+		String m = scanner.nextLine();
+		port = (m.equals("1")) ? SERVER_PORT : SIMULATOR_PORT;
 		
+		do {	
 		showInterface();	
 		
+		reset();
 
 		RequestPacket RPacket = new RequestPacket(request, filename, mode);
 
 		if (request.getType().equalsIgnoreCase("read")) {
-			new ClientRR(RPacket);
+			new ClientRR(RPacket, serverAddress, serverPort);
 		} else if (request.getType().equalsIgnoreCase("write")) {
-			new ClientWR(RPacket);
+			new ClientWR(RPacket, serverAddress, serverPort);
 		} else
 			System.out.println("Could not contact server");
 	
@@ -82,17 +84,10 @@ public class Client {
 	public void reset() {
 		if(sendReceiveSocket != null) sendReceiveSocket.close();
 		transmissionEnd = false;
-		request = null;
-		filename = null;
-		mode = null;
+		
 		originalPort = -1;
-		serverPort = SERVER_PORT;
-		try {
-			serverAddress = InetAddress.getLocalHost();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		serverPort = port;		
+		
 	}
 	
 	public Request createRequest(String type) {
@@ -112,7 +107,7 @@ public class Client {
 
 	private void showInterface() {
 		
-		System.out.println("Introduce the type of operation(type 1 for read, type 2 for write");
+		System.out.println("Introduce the type of operation(type 1 for read, type 2 for write)");
 		String value = scanner.nextLine();
 		String temp = "";
 		if(value.equals("1")) temp = "read";
@@ -124,7 +119,12 @@ public class Client {
 		filename = scanner.nextLine();
 		
 		System.out.println("Enter the mode");
-		mode = scanner.nextLine();		
+		mode = scanner.nextLine();
+		
+		System.out.println("MAKE SURE SERVER IS RUNNING AND LISTENING ON PORT 69");
+		System.out.println("IF WANT TO USE SIMULATOR MAKE SURE IT IS RUNNING AND LISTENING ON PORT 29");
+		System.out.println("\nPRESS ENTER WHEN YOU ARE READY");
+		scanner.nextLine();
 	}
 
 	public void endClientTransfer(String msj) {
@@ -185,12 +185,83 @@ public class Client {
 		transmissionEnd = true;
 		
 	}
+	
+	public void setServerAddress() {
+		System.out.println("Enter the server address or type 1 to set to localhost");
+		
+		hostName = scanner.nextLine();
+		
+		
+		try {
+			serverAddress = (hostName.equals("1")) ? InetAddress.getLocalHost():  InetAddress.getByName(hostName);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+	}
+	
+	public void test() {
+		String[] files = {"203p.jpg"};
+		
+		System.out.println("CLIENT");
+		setServerAddress();
+		System.out.println("Type 1 for Normal mode or type anything else for Simulator mode ");
+		port = (scanner.nextLine().equals("1")) ? SERVER_PORT : SIMULATOR_PORT;
+		
+		String tempRequest;
+		
+		for(int f = 	0; f < files.length; f++) {
+			
+			for(int i = 0; i < 28; i++) {		//28 non I/O possibles cases
+				if(serverAddress == null) setServerAddress();				
+				
+				if(i < 14) { //14 are on read / 14 are on write
+					tempRequest = "READ"; //readRequest			
+				}
+				else {
+					tempRequest = "WRITE"; //write				
+				}
+				
+				filename = files[f];
+				mode = "octet";
+				
+				System.out.println("\nPARAMETERS ARE SET TO DO: ");
+				System.out.println(tempRequest + " Request ");
+				System.out.println(filename + " filename ");
+				System.out.println(mode + " mode ");
+				
+				System.out.println("Press a enter to continue (MAKE SURE SIMULATOR IS READY)\n");
+				scanner.nextLine();
+				
+				request = createRequest(tempRequest);
+				
+				reset();
+				
+				RequestPacket RPacket = new RequestPacket(request , filename, mode);
+		
+				if (request.getType().equalsIgnoreCase("read")) {
+					new ClientRR(RPacket, serverAddress, port);
+				} else if (request.getType().equalsIgnoreCase("write")) {
+					new ClientWR(RPacket, serverAddress, port);
+				} else
+					System.out.println("Could not contact server");
+				
+				
+			}
+		}
+		System.exit(1);
+		scanner.close();
+		
+	}
 	/**
 	 * Main method of Client class
 	 */
 	public static void main(String args[]) {
 		Client client = new Client();
 		client.sendReceive();
+		
+		//client.test();
 	}
 
 }
